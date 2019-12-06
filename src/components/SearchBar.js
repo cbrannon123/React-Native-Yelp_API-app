e1dd3ce4445ad887a58bb5285cbf30d294933762
Casey Brannon <cbrannon412@gmail.com>
2019-12-06 18:41:51 -0500

    searcscreen and icons

---
+++ src/components/SearchBar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+//importing built in icons
+import { Feather} from '@expo/vector-icons';
+
+const SearchBar = () => {
+    return (
+        <View style={styles.background}>
+            <Feather name="search" size={30}/>
+            <Text>Search Bar</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({
+    background: {
+        backgroundColor: '#F0EEEE',
+        height: 50,
+        borderRadius: 5,
+        marginHorizontal: 15
+
+    }
+});
+
+export default SearchBar;
